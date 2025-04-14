5ae86d16caddaac3067d79d67750f557f69ba7df
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-14 23:26:44 +0530

    Create repeatString function as required.

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
-
+const repeatString = function(str, num) {
+    let repeatStr = "";
+    if(num >= 0){
+        for (let i = 1; i <= num ; i++){
+            repeatStr = repeatStr + str;
+        }
+        return repeatStr;
+    }else{
+        return "ERROR";
+    }
+    
 };
 
 // Do not edit below this line
