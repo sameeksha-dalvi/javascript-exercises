4dc2e61b24f37ddcf4d87e266b79b0149a526077
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-14 23:35:55 +0530

    Create function to reverse a string.

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,12 @@
-const reverseString = function() {
+const reverseString = function(text) {
+    let reverseText = "";
 
+    for(let i = text.length -1 ; i >= 0; i --){
+        
+        reverseText += text.charAt(i);
+    }
+
+    return reverseText;
 };
 
 // Do not edit below this line
