064fa0757c091ef246c7b180a96a19658fbbf79a
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-15 23:53:12 +0530

    Implement temparature conversion function.

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,18 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(value) {
+
+  let valueCelsius = (value - 32) * (5/9);
+
+  return parseFloat(valueCelsius.toFixed(1));
+
+
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(value) {
+
+  let valueFahrenheit = value * 9/5 + 32 ;
+
+  return parseFloat(valueFahrenheit.toFixed(1));
+
 };
 
 // Do not edit below this line
