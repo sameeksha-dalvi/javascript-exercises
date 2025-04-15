23c1c1d81ccf7d697d4e1255743eb675084608bc
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-15 23:10:11 +0530

    Implement sum of all numbers function.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,4 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
+
+    if(start < 0 || end < 0 || !Number.isInteger(start) || !Number.isInteger(end)){
+        return "ERROR";
+    }else{
+        if(start < end){
+            for(let i = start; i <= end ; i++){
+                sum+= i;
+            }
+        }else{
+            for(let i = start; i >= end ; i--){
+                sum+= i;
+            }
+        }
+    }
+   
+    
+
+    return sum;
+
 
 };
 
