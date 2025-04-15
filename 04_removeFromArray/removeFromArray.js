0b9e34dbc8f7ca42aa5c28da006250bbf8b43ebc
Sameeksha Dalvi <129096849+sameeksha-dalvi@users.noreply.github.com>
2025-04-15 20:24:37 +0530

    Implement remove from array elements function.

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,4 +1,16 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr , ...element) {
+let newArr = [];
+
+for (let i = 0 ; i < arr.length ; i++){
+    if(!element.includes(arr[i])){
+        newArr.push(arr[i]);
+    }
+}
+
+
+return newArr;
+
+
 };
 
 // Do not edit below this line
